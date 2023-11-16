db6f3d351c4b01b46a73700713e0b459b93d1815
Thabo Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2023-11-16 12:16:00 +0200

    First Commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+// Here we will create an express server
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+const port = 4000
+
+
+
+app.get('/login',(req, res) => {
+    res.send('Welcome to login page');
+});
+
+app.use('/',(req, res) => {
+    res.send('Hello World!');
+});
+
+
+// Define the database URL to connect to.
+const mongoDB = "mongodb+srv://tankiso:tKnQEZPlmAZDlNyQ@clustaphobia.pykrhce.mongodb.net/?retryWrites=true&w=majority";
+
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log("Connected Successfully");
+}
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
